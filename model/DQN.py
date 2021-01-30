10e4cbd92452a427a067ee2d730f73c54e6f0d8a
zengbohan0217 <13811753844@126.com>
2021-01-30 09:48:19 +0800

    first commit

---
+++ model/DQN.py
@@ -0,0 +1,3 @@
+import torch
+from GCN import *
+
---
+++ model/GCN.py
@@ -0,0 +1,51 @@
+import torch
+import torch.nn as nn
+import torch.nn.init as init
+import torch.nn.functional as F
+
+class GCNconv(nn.Module):
+    def __init__(self, in_c, out_c, bais=False):
+        super(GCNconv, self).__init__()
+        self.bais = bais
+        self.W = nn.Parameter(torch.FloatTensor(in_c, out_c))
+        init.xavier_uniform_(self.W, gain=1.44)
+        if self.bais:
+            self.B = nn.Parameter(torch.FloatTensor(1, out_c))
+            init.normal(self.B)
+
+    def forward(self, g_data,g_adj):
+        N = g_adj.size(0)  # N,N
+        B = g_data.size(0)  # B,N,F
+        # A\hat=A+I
+        g_adj = g_adj + torch.eye(N)
+        # D\hat^(-1/2)
+        degree = torch.diag(g_adj.sum(dim=1)) ** (-1 / 2)
+        # 0的-1/2要手动处理一下。
+        degree[degree == float("inf")] = 0
+        out = torch.mm(degree, g_adj)
+        out = torch.mm(out, degree)  # N,N
+        # matmul 可以自动扩展矩阵乘法。如下所示
+        out = torch.matmul(out, g_data)  # [N,N]*[B,N,in_C] =[B,N,int_c]
+        out = torch.matmul(out, self.W)  # [B,N,int_]*[int_c,out_c] =[B,N,out_c]
+        if self.bais:
+            out = out + self.B
+        return out
+
+class GCNnet(nn.Module):
+    def __init__(self, in_c, hid_c, out_c, bais=False):
+        super(GCNnet, self).__init__()
+        self.conv1 = GCNconv(in_c, hid_c, bais=bais)
+        self.conv2 = GCNconv(hid_c, out_c, bais=bais)
+
+        self.act = nn.ReLU()
+
+    def forward(self, data, device):
+        # data是输入数据，以字典的形式，需要有邻接矩阵和节点特征，由于这里的图都是一样的，所以只取第一个图。
+        graph = data["graph"].to(device)[0]  # [N,N]
+        flow_x = data["flow_x"].to(device)  # [B,N,D,H] 四个维度分别为： batch 节点 节点特征维度  H=1
+        B, N = flow_x.size(0), flow_x.size(1)
+        flow_x = flow_x.view(B, N, -1)  # [B, N, H*D]
+        out1 = self.act(self.conv1(flow_x, graph))
+        # print(out1.shape)
+        out2 = self.act(self.conv2(out1, graph))
+        return out2.unsqueeze(2)  # [B,N,1,1]
