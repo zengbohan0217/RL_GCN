0554e2c14dfa1e83fda05c636ff0e13acab8874c
zengbohan0217 <13811753844@126.com>
2021-02-01 09:54:59 +0800

    change the model

--- model/DQN.py
+++ model/DQN.py
@@ -26,15 +26,18 @@ class DQN:
         self.lr_shed = optim.lr_scheduler.StepLR(self.optimizer, 40, 0.8)
 
     def train_once(self, memory):
+        """
+        state所保留的信息包括了点的类别，不同类别对应地信息，当前请求运输所在位置，派发资源多少等，graph中点的类别包括了救助站、救助点
+        """
         self.model_train.train(True)
         self.optimizer.zero_grad()
         samples = random.sample(memory, self.batch_size)
         states_batch, action_batch, reward_batch, next_states_batch, _ = map(np.array, zip(*samples))
-        q_values_next_target = self.model_pred(next_states_batch).detach().numpy()
+        q_values_next_target = self.model_pred(self.graph, next_states_batch).detach().numpy()
         targets_batch = reward_batch + self.gamma * q_values_next_target.max(axis=1)
         targets_batch = np.maximum(-5, targets_batch)
         targets_batch = torch.FloatTensor(targets_batch.reshape(-1, 1))
-        q_values_next_pred = self.model_train(states_batch)
+        q_values_next_pred = self.model_train(self.graph, states_batch)
         action_batch = torch.tensor(action_batch.reshape(-1, 1), dtype=torch.int64)
         pred_batch = q_values_next_pred.gather(-1, action_batch)
         # train model
